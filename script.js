dfba387d54017395354fc969aafa81a1f4685e6e
Jens Tschirpig <jens1101@hotmail.com>
2018-06-20 09:28:15 +0200

    Code cleanup - Used classes in the HTML instead of IDs - Used event handlers in JS for starting and stopping pomodoros - Reformatted the JS to comply with standardJS

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
+/* globals Audio, Notification */
+
 // TODO: Maybe use local storage so that the pomodoro doesn't get lost when closing the tab.
 // TODO: Add the ability to add distractions
 
 let interval = null
 const COUNTDOWN_SECONDS = 25 * 60 // 25 minutes
-// const COUNTDOWN_SECONDS = 3
-const notificationSound = new Audio();
+const notificationSound = new Audio()
 notificationSound.src = 'https://www.myinstants.com/media/sounds/its-time-to-stop-button.mp3'
 
-function startPomodoro() {
-    if (interval) {
-        stopPomodoro()
-    }
-    
-    let seconds = COUNTDOWN_SECONDS
+const startPomodoroButton = document.querySelector('.start-pomodoro')
+const stopPomodoroButton = document.querySelector('.stop-pomodoro')
+const countdown = document.querySelector('.countdown')
+if (startPomodoroButton) {
+  startPomodoroButton.addEventListener('click', startPomodoro)
+}
+if (stopPomodoroButton) {
+  stopPomodoroButton.addEventListener('click', stopPomodoro)
+}
+
+function startPomodoro () {
+  if (interval) {
+    stopPomodoro()
+  }
+
+  let seconds = COUNTDOWN_SECONDS
+  displayTime(seconds)
+
+  interval = setInterval(() => {
+    --seconds
     displayTime(seconds)
 
-    interval = setInterval(() => {
-        --seconds
-        displayTime(seconds)
-        
-        if(seconds <= 0) {
-            stopPomodoro()
-            showNotification()
-        }
-    }, 1000);
-}
-
-function stopPomodoro() {
-    clearInterval(interval)
-    interval = null
-    document.getElementById("countdown").innerHTML = ''
-}
-
-function displayTime(seconds) {
-    let minutes = Math.floor(seconds / 60);
-    let _seconds = Math.floor(seconds % 60);
-    
-    if (minutes < 10) { minutes = `0${minutes}` }
-    if (_seconds < 10) { _seconds = `0${_seconds}` }
-    
-    // Display the result in the element with id="demo"
-    document.getElementById("countdown").innerHTML = `${minutes}:${_seconds}`
-}
-
-function showNotification() {
-    if (Notification.permission === "granted") {
-        var notification = new Notification("Time is up!");
-        notificationSound.play()
+    if (seconds <= 0) {
+      stopPomodoro()
+      showNotification()
     }
+  }, 1000)
+}
+
+function stopPomodoro () {
+  clearInterval(interval)
+  interval = null
+  countdown.innerHTML = ''
+}
+
+function displayTime (seconds) {
+  let minutes = Math.floor(seconds / 60)
+  let _seconds = Math.floor(seconds % 60)
+
+  if (minutes < 10) { minutes = `0${minutes}` }
+  if (_seconds < 10) { _seconds = `0${_seconds}` }
+
+  countdown.innerHTML = `${minutes}:${_seconds}`
+}
+
+function showNotification () {
+  if (Notification.permission === 'granted') {
+    Notification('Time is up!')
+  }
+  notificationSound.play()
 }

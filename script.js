4f3719949578d15a5e6767e33a637fea708b0fbe
Jens Tschirpig <jens1101@hotmail.com>
2018-06-28 15:28:55 +0200

    Implemented countdown timer properly - Added events to the custom element - Updated HTML - Updated script to use the new events

--- script.js
+++ script.js
@@ -3,15 +3,11 @@
 /* globals Audio, Notification */
 
 // TODO: Maybe use local storage so that the pomodoro doesn't get lost when closing the tab.
-// TODO: Add the ability to add distractions
 
-let interval = null
 const COUNTDOWN_SECONDS = 25 * 60 // 25 minutes
 const notificationSound = new Audio()
 
-const startPomodoroButton = document.querySelector('.start-pomodoro')
-const stopPomodoroButton = document.querySelector('.stop-pomodoro')
-const countdown = document.querySelector('.countdown')
+const countdown = document.getElementById('countdown')
 const distractionForm = document.querySelector('.add-distraction')
 const distractionsList = document.querySelector('.distractions')
 
@@ -20,8 +16,9 @@ init()
 function init () {
   notificationSound.src = 'assets/audio/pomodoro-over.mp3'
 
-  startPomodoroButton.addEventListener('click', startPomodoro)
-  stopPomodoroButton.addEventListener('click', stopPomodoro)
+  countdown.setAttribute('duration', COUNTDOWN_SECONDS)
+  countdown.addEventListener('countdowncomplete', showNotification)
+
   distractionForm.addEventListener('submit', function (event) {
     event.preventDefault()
 
@@ -36,41 +33,6 @@ function init () {
     // Request notification permission if it hasn't been explicitly granted or denied.
     Notification.requestPermission()
   }
-}
-
-function startPomodoro () {
-  if (interval) {
-    stopPomodoro()
-  }
-
-  let seconds = COUNTDOWN_SECONDS
-  displayTime(seconds)
-
-  interval = setInterval(() => {
-    --seconds
-    displayTime(seconds)
-
-    if (seconds <= 0) {
-      stopPomodoro()
-      showNotification()
-    }
-  }, 1000)
-}
-
-function stopPomodoro () {
-  clearInterval(interval)
-  interval = null
-  countdown.innerHTML = ''
-}
-
-function displayTime (seconds) {
-  let minutes = Math.floor(seconds / 60)
-  let _seconds = Math.floor(seconds % 60)
-
-  if (minutes < 10) { minutes = `0${minutes}` }
-  if (_seconds < 10) { _seconds = `0${_seconds}` }
-
-  countdown.innerHTML = `${minutes}:${_seconds}`
 }
 
 function addDistraction (distraction) {

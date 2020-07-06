15e96f5dc01ac72a0f2f471d4fca6ed0528592fa
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-07-06 08:38:27 +0200

    Added font awesome

--- src/EditableList.js
+++ src/EditableList.js
@@ -1,3 +1,5 @@
+import { faTimes, faPlus } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 
 /**
@@ -5,8 +7,10 @@ import React, { useState } from "react";
  * @param {Object} props
  * @param {string} props.placeholder The placeholder to display in the text
  * input element.
- * @param {string|React.Component} [props.addButton = 'Add'] The text or
- * component to display in the add button.
+ * @param {string|React.Component} [props.addButton] The text or
+ * component to display in the add item button.
+ * @param {string|React.Component} [props.removeButton] The text or
+ * component to display in the remove item button.
  * @returns {*}
  */
 export function EditableList(props) {
@@ -19,8 +23,20 @@ export function EditableList(props) {
     .map((item, index) => (
       <li className={"list-group-item d-flex align-items-center"} key={index}>
         <span className={"flex-grow-1"}>{item.text}</span>
+        <button
+          type={"button"}
+          className={"btn btn-outline-danger btn-sm"}
+          onClick={() => removeItem(item)}
+          title={"Remove item"}
+        >
+          {props.removeButton || <FontAwesomeIcon icon={faTimes} />}
+        </button>
       </li>
     ));
+
+  function removeItem(item) {
+    setItems(items.filter((currentItem) => currentItem !== item));
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -47,7 +63,7 @@ export function EditableList(props) {
           />
           <div className={"input-group-append"}>
             <button className={"btn btn-primary"} type={"submit"}>
-              {props.addButton || "Add"}
+              {props.addButton || <FontAwesomeIcon icon={faPlus} />}
             </button>
           </div>
         </div>

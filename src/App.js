9b40fd17be00e0f798a322bc9547ad8ae4ec31a6
Jens Tschirpig <jens1101@users.noreply.github.com>
2020-07-06 08:38:27 +0200

    Added font awesome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import { EditableList } from "./EditableList.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+
+const addDistractionText = (
+  <>
+    <FontAwesomeIcon icon={faPlus} /> Add
+  </>
+);
 
 export function App() {
   return (
     <div className={"container"}>
-      <EditableList placeholder={"Note a distraction"} />
+      <EditableList
+        placeholder={"Note a distraction"}
+        addButton={addDistractionText}
+      />
     </div>
   );
 }
